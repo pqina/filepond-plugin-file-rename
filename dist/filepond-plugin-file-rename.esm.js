dad1885160df0201113f068c708b9e10c311b5c5
Rik Schennink <rikschennink@gmail.com>
2018-06-04 16:26:56 +0200

    initial commit

---
+++ dist/filepond-plugin-file-rename.esm.js
@@ -0,0 +1,61 @@
+/*
+ * FilePondPluginFileRename 1.0.0
+ * Licensed under MIT, https://opensource.org/licenses/MIT
+ * Please visit https://pqina.nl/filepond for details.
+ */
+var plugin$1 = ({ addFilter, utils }) => {
+  // get quick reference to Type utils
+  const {
+    Type,
+    renameFile,
+    getExtensionFromFilename,
+    getFilenameWithoutExtension
+  } = utils;
+
+  // called for each file that is loaded
+  // right before it is set to the item state
+  // should return a promise
+  addFilter(
+    'LOAD_FILE',
+    (file, { query }) =>
+      new Promise((resolve, reject) => {
+        // reject
+        const allowFileRename = query('GET_ALLOW_FILE_RENAME');
+        const renameFunction = query('GET_FILE_RENAME_FUNCTION');
+        if (!allowFileRename || !renameFunction) {
+          resolve(file);
+          return;
+        }
+
+        resolve(
+          renameFile(
+            file,
+            renameFunction({
+              name: file.name,
+              basename: getFilenameWithoutExtension(file.name),
+              extension: `.${getExtensionFromFilename(file.name)}`
+            })
+          )
+        );
+      })
+  );
+
+  return {
+    options: {
+      // Enable or disable file renaming
+      allowFileRename: [true, Type.BOOLEAN],
+
+      // Rename function to run for this
+      fileRenameFunction: [null, Type.FUNCTION]
+    }
+  };
+};
+
+if (typeof navigator !== 'undefined' && document) {
+  // plugin has loaded
+  document.dispatchEvent(
+    new CustomEvent('FilePond:pluginloaded', { detail: plugin$1 })
+  );
+}
+
+export default plugin$1;
---
+++ dist/filepond-plugin-file-rename.js
@@ -0,0 +1,72 @@
+/*
+ * FilePondPluginFileRename 1.0.0
+ * Licensed under MIT, https://opensource.org/licenses/MIT
+ * Please visit https://pqina.nl/filepond for details.
+ */
+(function(global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined'
+    ? (module.exports = factory())
+    : typeof define === 'function' && define.amd
+      ? define(factory)
+      : (global.FilePondPluginFileRename = factory());
+})(this, function() {
+  'use strict';
+
+  var plugin$1 = function(_ref) {
+    var addFilter = _ref.addFilter,
+      utils = _ref.utils;
+
+    // get quick reference to Type utils
+    var Type = utils.Type,
+      renameFile = utils.renameFile,
+      getExtensionFromFilename = utils.getExtensionFromFilename,
+      getFilenameWithoutExtension = utils.getFilenameWithoutExtension;
+
+    // called for each file that is loaded
+    // right before it is set to the item state
+    // should return a promise
+
+    addFilter('LOAD_FILE', function(file, _ref2) {
+      var query = _ref2.query;
+      return new Promise(function(resolve, reject) {
+        // reject
+        var allowFileRename = query('GET_ALLOW_FILE_RENAME');
+        var renameFunction = query('GET_FILE_RENAME_FUNCTION');
+        if (!allowFileRename || !renameFunction) {
+          resolve(file);
+          return;
+        }
+
+        resolve(
+          renameFile(
+            file,
+            renameFunction({
+              name: file.name,
+              basename: getFilenameWithoutExtension(file.name),
+              extension: '.' + getExtensionFromFilename(file.name)
+            })
+          )
+        );
+      });
+    });
+
+    return {
+      options: {
+        // Enable or disable file renaming
+        allowFileRename: [true, Type.BOOLEAN],
+
+        // Rename function to run for this
+        fileRenameFunction: [null, Type.FUNCTION]
+      }
+    };
+  };
+
+  if (typeof navigator !== 'undefined' && document) {
+    // plugin has loaded
+    document.dispatchEvent(
+      new CustomEvent('FilePond:pluginloaded', { detail: plugin$1 })
+    );
+  }
+
+  return plugin$1;
+});
