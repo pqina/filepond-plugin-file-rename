e8d5a9d3177431c511775b355e6aeafbf015c207
Sebastian Busch <devel@sebastian-bus.ch>
2020-07-17 09:17:57 +0200

    add types

---
+++ types/index.d.ts
@@ -0,0 +1,18 @@
+declare module "filepond-plugin-file-rename" {
+    const FilepondPluginFileRename: FilepondPluginFilepondPluginFileRenameProps;
+    export interface FilepondPluginFilepondPluginFileRenameProps {
+        /** Enable or disable file renaming */
+        allowFileRename: boolean;
+
+        /** A function that receives an objecting containing file information like basename, extension and name. It should return either a string value or a Promise that resolves with a string value. */ 
+        fileRenameFunction: undefined | null | (
+            { 
+                basename: string;
+                extension: string;
+                name: string;
+                // is this complete?
+            }
+        ) => string | Promise<string>;
+    }
+    export default FilepondPluginFileRename;
+}%
