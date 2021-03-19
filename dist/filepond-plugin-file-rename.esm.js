d9a31e8fd04705116fb75b65e1ff85cfbbb04c39
Rik Schennink <rikschennink@gmail.com>
2021-03-19 16:19:10 +0100

    make props optional

--- dist/filepond-plugin-file-rename.esm.js
+++ dist/filepond-plugin-file-rename.esm.js
@@ -1,5 +1,5 @@
 /*!
- * FilePondPluginFileRename 1.1.7
+ * FilePondPluginFileRename 1.1.8
  * Licensed under MIT, https://opensource.org/licenses/MIT/
  * Please visit https://pqina.nl/filepond/ for details.
  */
--- types/index.d.ts
+++ types/index.d.ts
@@ -4,10 +4,10 @@ import { FilePondOptions } from "filepond";
 declare module "filepond" {
   export interface FilePondOptions {
     /** Enable or disable file renaming */
-    allowFileRename: boolean;
+    allowFileRename?: boolean;
 
     /** A function that receives an objecting containing file information like basename, extension and name. It should return either a string value or a Promise that resolves with a string value. */
-    fileRenameFunction:
+    fileRenameFunction?:
       | undefined
       | null
       | ((options: {
